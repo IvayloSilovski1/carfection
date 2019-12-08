66c4a2a7b7290f8f8541bf0bb1a35b1744ed517f
is5829s <ivaylo.silovski@alumni.fh-aachen.de>
2019-12-08 10:44:02 +0100

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 // import mongoose
 const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname+'/views');
@@ -18,6 +20,9 @@ app.set('views', __dirname+'/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+
+// body-parser
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 // connect to a database
 mongoose.connect(process.env.DATABASE_URL, {
@@ -30,6 +35,7 @@ db.once('open', ()=>console.log('Connected to MongoDB'));
 
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 
 app.listen(process.env.PORT || 3000, ()=>console.log('Server running on server 3000'));
